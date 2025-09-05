8fb1f96aff22904c922fb0c535425ca2cf1f3152
smgzach <zm989392@gmail.com>
2025-09-05 15:55:58 -0400

    init commit

---
+++ src/main/java/com/zachprogramming/carcomparisonwebsite/CarComparisonWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.zachprogramming.carcomparisonwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarComparisonWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarComparisonWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zachprogramming/carcomparisonwebsite/CarComparisonWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachprogramming.carcomparisonwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarComparisonWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
