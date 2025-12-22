d443d3adb592ec2e8f48bf49586b5df62d0c935c
smgzach <zm989392@gmail.com>
2025-12-22 12:58:09 -0500

    docker works, changed the dummy data to only load if there is not already data in the DB

--- src/main/java/com/zachprogramming/carcomparisonwebsite/CarComparisonWebsiteApplication.java
+++ src/main/java/com/zachprogramming/carcomparisonwebsite/CarComparisonWebsiteApplication.java
@@ -19,13 +19,20 @@ public class CarComparisonWebsiteApplication {
     {
         return args ->
         {
-            carRepository.save(new Car("Honda", "Civic", 2020, "White", 20000, 158, 34, "LX"));
-            carRepository.save(new Car("Nissan", "Rogue", 2013, "Black", 1500, 170, 25, "S"));
-            carRepository.save(new Car("Ford", "Fusion", 2016, "White", 17000, 230, 25, "Titanium"));
-            carRepository.save(new Car("Ford", "Mustang", 2025, "Black", 45000, 460, 18, "Premium"));
-            carRepository.save(new Car("Toyota", "Supra", 1996, "Blue", 20000, 1000, 15, "Sport"));
-            carRepository.save(new Car("Ford", "F-150", 2025, "White", 40000, 400, 18, "super"));
-            carRepository.save(new Car("Toyota", "Prius", 2020, "Red", 25000, 120, 52, "Base"));
+            if(carRepository.count()==0)
+            {
+                carRepository.save(new Car("Honda", "Civic", 2020, "White", 20000, 158, 34, "LX"));
+                carRepository.save(new Car("Nissan", "Rogue", 2013, "Black", 1500, 170, 25, "S"));
+                carRepository.save(new Car("Ford", "Fusion", 2016, "White", 17000, 230, 25, "Titanium"));
+                carRepository.save(new Car("Ford", "Mustang", 2025, "Black", 45000, 460, 18, "Premium"));
+                carRepository.save(new Car("Toyota", "Supra", 1996, "Blue", 20000, 1000, 15, "Sport"));
+                carRepository.save(new Car("Ford", "F-150", 2025, "White", 40000, 400, 18, "super"));
+                carRepository.save(new Car("Toyota", "Prius", 2020, "Red", 25000, 120, 52, "Base"));
+            }
+            else
+            {
+                System.out.println("Database already contains cars. Skipping...");
+            }
         };
     }
 }
